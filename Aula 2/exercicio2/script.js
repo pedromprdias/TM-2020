268203977d5ea4cc6f6120746ce1a85305f71780
pedromprdias <61282654+pedromprdias@users.noreply.github.com>
2020-03-05 14:45:41 +0000

    PedroDias_21554

---
+++ Aula 2/exercicio2/script.js
@@ -0,0 +1,19 @@
+var  arrayNumbers = [];
+
+function lerInput() {
+    var numero = document.getElementById("numbersId").value;
+
+    arrayNumbers.push(parseInt(numero));
+
+    document.getElementById("NumAdd").innerText = arrayNumbers.toString();
+}
+
+function calcularNumero() {
+    if(arrayNumbers.length >= 5){
+        var maior = Math.max.apply(Math,arrayNumbers);
+        alert(maior);
+    }else{
+        alert("É necessário inserir 5 digitos");
+    }
+
+}
